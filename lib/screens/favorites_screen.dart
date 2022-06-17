691e37a4b211ff57412c4c1f1d4386556dc859a9
Marek Woźniak <marek.wozniak@microsoft.wsei.edu.pl>
2022-06-17 02:45:13 +0200

    Filters + Favs

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/meal.dart';
+import 'package:flutter_complete_guide/widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  static const routeName = '/favorites';
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - start adding some!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            duration: favoriteMeals[index].duration,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
